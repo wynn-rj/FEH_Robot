0e991a8fe486d8645e4db0af5ca26a78ae9cf666
Robert J Wynn <wynn.134@osu.edu>
2017-02-07 15:10:09 -0500

    Comments and Initial Log Setup
    Setup the SD logging system and added comments to preexisting code

--- main.cpp
+++ main.cpp
@@ -1,13 +1,25 @@
 #include <FEHLCD.h>
 #include <FEHIO.h>
 #include <FEHUtility.h>
+#include <FEHSD.h>
 
 int main(void)
 {
-    LCD.Clear( FEHLCD::Black );
-    LCD.SetFontColor( FEHLCD::White );
+    //CLears the LCD of anything previously on it
+    LCD.Clear(FEHLCD::Black);
+    LCD.SetFontColor(FEHLCD::White);
 
-    LCD.WriteLine( "Libertatum Volcanus" );
+    //Starting SD log file
+    SD.OpenLog();
+    SD.Printf("Initializing Log");
 
+    //To display on screen. Probably will be changed later
+    LCD.WriteLine("Libertatum Volcanus");
+
+    //Closing SD log file
+    SD.Printf("Closing Log");
+    SD.CloseLog();
+
+    //Ending Program
     return 0;
 }
