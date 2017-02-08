14dcc6e702188243849a25a8ede7ec15cdac2fe8
wynn-rj <wynn.134@osu.edu>
2017-02-08 14:28:56 -0500

    Test
    Testing to see if I have to make a commit to be able to build code

--- main.cpp
+++ main.cpp
@@ -12,16 +12,38 @@
 #define SHOW_C BLUE
 #define HI_C GREEN
 
-/* Define menu number values */
-#define MN_MENU 0
-
 /* Define time for beep */
 #define beep_t 10 // int milliseconds
 
-int initMenu();
+/////////////////////////////////////REGION: Declerations/////////////////////////////////////
 
+/* Structure declerations */
+
+//Course Struct used to for proteus to know what course it is on and extra info for that course
+typedef struct Course
+{
+public:
+    Course(char);
+
+private:
+    char courseLetter;
+} Course;
+
+/* Function declerations */
+
+Course initMenu();
+
+//////////////////////////////////////////END REGION//////////////////////////////////////////
+
+/**
+ * @brief main
+ *      main program method
+ * @return 0
+ */
 int main(void)
 {
+    Course currentCourse;
+
     //CLears the LCD of anything previously on it
     LCD.Clear(FEHLCD::Black);
     LCD.SetFontColor(FEHLCD::White);
@@ -31,7 +53,8 @@ int main(void)
     SD.OpenLog();
     SD.Printf("Initializing Log");
 
-    initMenu();
+    //Determine course robot located on
+    currentCourse = initMenu();
 
     //Closing SD log file
     SD.Printf("Closing Log");
@@ -41,11 +64,23 @@ int main(void)
     return 0;
 }
 
-int initMenu()
+/**
+ * @brief initMenu
+ *       This method is used before running the course so that
+ *       the robot knows which of the 8 courses it is on
+ */
+Course initMenu()
 {
+    /* Variable Decleration */
+    bool noCourseSelected = true;
+    int n;
+    float x, y;
+    float m = 0, bat_v = 0;
+    Course selectedCourse;
+
     LCD.Clear(BLACK);
 
-    /* Create icons for main menu */
+    //Create icons for main menu
     FEHIcon::Icon MAIN_T[1];
     char main_t_label[1][20] = {"LIBERTATUM VOLCANUS"};
     FEHIcon::DrawIconArray(MAIN_T, 1, 1, 1, 201, 1, 1, main_t_label, HI_C, TEXT_C);
@@ -61,33 +96,53 @@ int initMenu()
 
     Buzzer.Buzz(beep_t);
 
-    int menu=MN_MENU, n;
-    float x, y;
-    float m = 0, bat_v = 0;
 
-    while(menu==MN_MENU)
+    while(noCourseSelected)
     {
-        /* Display average battery voltage to screen */
+        //Display average battery voltage to screen
         bat_v = ((bat_v*m)+Battery.Voltage());
         bat_v = bat_v/(++m);
         LCD.WriteAt(bat_v, 72, 222);
         if (LCD.Touch(&x, &y))
         {
-            /* Check to see if a main menu icon has been touched */
+            //Check to see if a main menu icon has been touched
             for (n=0; n<=7; n++)
             {
                 if (MAIN[n].Pressed(x, y, 0))
                 {
+                    //States selected course to screen and log
                     LCD.Clear(BLACK);
                     LCD.WriteLine("Course   Selected");
                     LCD.WriteRC(main_label[n], 0, 7);
                     SD.Printf("Selecting Course: ");
                     SD.Printf(main_label[n]);
                     SD.Printf("\n");
+
+                    selectedCourse = new Course(main_label[n][0]);
+
+                    noCourseSelected = false;
                     break;
                 }
             }
         }
     }
-    return menu;
 }
+
+/////////////////////////////////////REGION: Struct Definitions/////////////////////////////////////
+
+/**
+ * @brief Course::Course
+ *      Constructor for the Course Struct
+ * @param courseLet
+ *      A char that is used to define which
+ *      course the robot is starting on
+ */
+Course::Course(char courseLet)
+{
+    Course::courseLetter = courseLet;
+}
+
+/////////////////////////////////////////////END REGION/////////////////////////////////////////////
+
+
+,
