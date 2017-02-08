29dfb54d2d45a8617e7b3362cfa261f3367a2da9
Robert J Wynn <wynn.134@osu.edu>
2017-02-08 09:57:44 -0500

    Updated Main

--- main.cpp
+++ main.cpp
@@ -3,6 +3,8 @@
 #include <FEHUtility.h>
 #include <FEHSD.h>
 
+int initMenu();
+
 int main(void)
 {
     //CLears the LCD of anything previously on it
@@ -13,8 +15,7 @@ int main(void)
     SD.OpenLog();
     SD.Printf("Initializing Log");
 
-    //To display on screen. Probably will be changed later
-    LCD.WriteLine("Libertatum Volcanus");
+    initMenu();
 
     //Closing SD log file
     SD.Printf("Closing Log");
@@ -22,4 +23,55 @@ int main(void)
 
     //Ending Program
     return 0;
+}
+
+int initMenu()
+{
+    LCD.Clear(BLACK);
+
+    /* Create icons for main menu */
+    FEHIcon::Icon MAIN_T[1];
+    char main_t_label[1][20] = {"LIBERTATUM VOLCANUS"};
+    FEHIcon::DrawIconArray(MAIN_T, 1, 1, 1, 201, 1, 1, main_t_label, HI_C, TEXT_C);
+    MAIN_T[0].Select();
+
+    FEHIcon::Icon MAIN[6];
+    char main_label[8][20] = {"A", "B", "C", "D", "E", "F", "G", "H"};
+    FEHIcon::DrawIconArray(MAIN, 4, 2, 40, 20, 1, 1, main_label, MENU_C, TEXT_C);
+
+
+    LCD.SetFontColor(TEXT_C);
+    LCD.WriteAt("BATT:        V", 0, 222);
+
+    Buzzer.Buzz(beep_t);
+
+    int menu=MN_MENU, n;
+    float x, y;
+    float m = 0, bat_v = 0;
+
+    while(menu==MN_MENU)
+    {
+        /* Display average battery voltage to screen */
+        bat_v = ((bat_v*m)+Battery.Voltage());
+        bat_v = bat_v/(++m);
+        LCD.WriteAt(bat_v, 72, 222);
+        if (LCD.Touch(&x, &y))
+        {
+            /* Check to see if a main menu icon has been touched */
+            for (n=0; n<=7; n++)
+            {
+                if (MAIN[n].Pressed(x, y, 0))
+                {
+                    LCD.Clear(BLACK);
+                    LCD.WriteLine("Course   Selected");
+                    LCD.WriteRC(main_label[n], 0, 7);
+                    SD.Printf("Selecting Course: ");
+                    SD.Printf(main_label[n]);
+                    SD.Printf("\n");
+                    break;
+                }
+            }
+        }
+    }
+    return menu;
 }
