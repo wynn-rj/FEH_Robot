b207ccb3d463f12a6bf2cd73803fa2543423a75e
Robert J Wynn <wynn.134@osu.edu>
2017-02-08 11:35:54 -0500

    Bugs Fixed
    Fixed Qt creator not writing code properly

--- main.d
+++ main.d
@@ -13,7 +13,13 @@
  C:\fehqt\Proteus/EWL/EWL_C/include/limits_api.h \
  C:\fehqt\Proteus/EWL/EWL_C/include/ewl_lib_ext1.h Libraries/FEHUtility.h \
  Startup/derivative.h Libraries/LCDColors.h Libraries/FEHIO.h \
- Drivers/adc16.h
+ Drivers/adc16.h Libraries/FEHSD.h Drivers/SDHC.h Drivers/diskio.h \
+ Drivers/ff.h C:\fehqt\Proteus/EWL/EWL_C/include/stdarg.h \
+ C:\fehqt\Proteus/EWL/EWL_C/include/cstdarg \
+ C:\fehqt\Proteus/EWL/EWL_C/include/va_list.h \
+ C:\fehqt\Proteus/EWL/EWL_C/include/arm/stdarg.ARM.h Drivers/ffconf.h \
+ Drivers/ffconf.h Drivers/diskio.h Libraries/FEHBattery.h \
+ Libraries/FEHIO.h Libraries/FEHBuzzer.h
 
 C:\fehqt\Proteus/EWL/EWL_C++/include/lib_ewl_c++.prefix:
 
@@ -56,3 +62,31 @@ Libraries/LCDColors.h:
 Libraries/FEHIO.h:
 
 Drivers/adc16.h:
+
+Libraries/FEHSD.h:
+
+Drivers/SDHC.h:
+
+Drivers/diskio.h:
+
+Drivers/ff.h:
+
+C:\fehqt\Proteus/EWL/EWL_C/include/stdarg.h:
+
+C:\fehqt\Proteus/EWL/EWL_C/include/cstdarg:
+
+C:\fehqt\Proteus/EWL/EWL_C/include/va_list.h:
+
+C:\fehqt\Proteus/EWL/EWL_C/include/arm/stdarg.ARM.h:
+
+Drivers/ffconf.h:
+
+Drivers/ffconf.h:
+
+Drivers/diskio.h:
+
+Libraries/FEHBattery.h:
+
+Libraries/FEHIO.h:
+
+Libraries/FEHBuzzer.h:
